a5bb86d871604ac379820fdd7509b8e5a387c391
Tomas Doran <bobtfish@bobtfish.net>
2009-10-05 20:30:02 +0100

    MI::CheckConflicts, bump version for release

--- lib/Text/MultiMarkdown.pm
+++ lib/Text/MultiMarkdown.pm
@@ -9,7 +9,7 @@ use Encode      qw();
 use Carp        qw(croak);
 use base        qw(Text::Markdown);
 
-our $VERSION   = '1.0.27';
+our $VERSION   = '1.0.28';
 our @EXPORT_OK = qw(markdown);
 
 =head1 NAME
